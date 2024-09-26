e35abc7258ccb922502da6c84432cb2cf74d5b27
wieginebabyko120409 <wiegine@ech.dev>
2024-09-26 12:16:43 +0800

    update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(require("./corss"));
 app.use(express.json());
 app.set("json spaces", 4);
 function userAgent() {
